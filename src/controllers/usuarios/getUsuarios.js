6c3c2830a6f1b6c46ce64a205e173c908d57927d
Nelson Ramírez <xhyskoax@gmail.com>
2026-08-31 11:01:12 -0400

    Se agregan filtros a controlador getAllUsuarios a través de la queryString

--- src/controllers/usuarios/getUsuarios.js
+++ src/controllers/usuarios/getUsuarios.js
@@ -1,15 +1,47 @@
 import Usuario from "../../models/Usuario.model.js";
 
-const getUsuarios= async (req, res) => {
+const getUsuarios = async (req, res) => {
     try {
 
-        const usuarios = await Usuario.findAll({
-            attributes: ["id", "nombre", "email"]
+        let { offset, limit, sortBy, direction } = req.query;
+
+        if(offset) {
+            offset = Number(offset);
+        }
+
+        if(limit){
+            limit = Number(limit);
+        }
+
+        const opcionesOrden = ["id", "nombre", "email"];
+        const order = [];
+
+        if(sortBy && opcionesOrden.includes(sortBy)){
+            sortBy = sortBy.toLowerCase().trim();
+
+            let orden;
+            if(direction && direction.toLowerCase().trim() == "desc"){
+                orden = [sortBy, "DESC"];
+            }else {
+                orden = [sortBy, "ASC"];
+            }
+            
+            order.push(orden);
+        }
+
+        
+
+        const { count, rows } = await Usuario.findAndCountAll({
+            attributes: ["id", "nombre", "email"],
+            offset: isNaN(offset) ? undefined : offset,
+            limit: isNaN(limit) ? undefined : limit,
+            order
         });
 
         res.json({
             status: "Ok",
-            usuarios,
+            totalUsuariosDb: count,
+            usuarios: rows,
         });
     } catch (error) {
         res.status(500).json({ status: "error", message: error.message });
