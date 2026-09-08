cb74d88fbd694793264677ba7bca3c2f7c6c86d2
Nelson Ramírez <xhyskoax@gmail.com>
2026-09-08 10:18:13 -0300

    Se crea vista home mostrando publicaciones y se crea partial statsCard

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,18 +1,41 @@
 import express from "express";
+import Usuario from "../models/Usuario.model.js";
+import Publicacion from "../models/Publicacion.model.js";
+import Comentario from "../models/Comentario.model.js";
+import moment from "moment";
 
 const router = express.Router();
 
 //VISTA HOME
-router.get(["/"], (req, res) => {
+router.get(["/"], async (req, res) => {
     try {
+        const { count, rows } = await Publicacion.findAndCountAll({
+            include: [
+                {
+                    model: Usuario,
+                    as: "autor",
+                    attributes: ["id", "nombre", "email"],
+                },
+            ],
+        });
 
-        res.render("home");
+        const publicaciones = rows.map((p) => {
+            p = p.toJSON();
+            p.fechaCreacion = moment(p.fechaCreacion).format("DD/MM/YYYY hh:mm:ss a");
+            p.fechaActualizacion = moment(p.fechaActualizacion).format("DD/MM/YYYY hh:mm:ss a");
+            return p;
+        });
+        const cantidadPublicaciones = count;
 
+        console.log(publicaciones);
+
+        res.render("home", {
+            publicaciones, cantidadPublicaciones
+        });
     } catch (error) {
         console.log(error);
         res.render("home");
     }
 });
-
 
 export default router;
